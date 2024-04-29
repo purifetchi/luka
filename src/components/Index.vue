836d3468df90a157bc95303f17d0392d42e36cb3
purifetchi <0xlunaric@gmail.com>
2024-04-29 17:04:18 +0200

    chore: bring in tailwindcss and flowbite.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,6 +3,7 @@
 import Login from "@/components/Login.vue";
 import { store } from "@/store/store.js";
 import UserInformation from "@/components/UserInformation.vue";
+import {FwbDropdown, FwbListGroup, FwbListGroupItem} from "flowbite-vue";
 
 </script>
 
@@ -10,6 +11,13 @@ import UserInformation from "@/components/UserInformation.vue";
   <h1>Luka: The Toki Frontend</h1>
   <p>Something will be here, I promise.</p>
   <br>
+  <fwb-dropdown text="Click me" placement="top">
+    <fwb-list-group>
+      <fwb-list-group-item>Item #1</fwb-list-group-item>
+      <fwb-list-group-item>Item #2</fwb-list-group-item>
+      <fwb-list-group-item>Item #3</fwb-list-group-item>
+    </fwb-list-group>
+  </fwb-dropdown>
   <Login v-if="store.client_token === null"/>
   <UserInformation v-else />
 </template>
