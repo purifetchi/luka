d318f75aa9f0168188dbc145fbc4442154700f44
purifetchi <0xlunaric@gmail.com>
2024-04-29 22:27:55 +0200

    refactor: move postcontent out of the post container.

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,29 +1,37 @@
 <script lang="ts" setup>
 import { Status } from "@/api/entities/status";
 import { FwbAvatar } from "flowbite-vue";
+import PostContent from "@/components/PostContent.vue";
+import { onMounted, ref } from "vue";
 
 const props = defineProps<{
   status: Status
 }>();
+
+const baseStatus = ref<Status>(null);
+onMounted(() => {
+  baseStatus.value = props.status.reblog !== null ?
+      props.status.reblog :
+      props.status;
+});
+
 </script>
 
 <template>
-  <article class="p-5 bg-slate-700 space-y-2">
-    <div class="flex items-center space-x-2 rtl:space-x-reverse">
-      <FwbAvatar size="md" :img="props.status.account.avatar" />
-      <div class="space-y-1 font-medium dark:text-white">
-        <b><RouterLink :to="`/user/${props.status.account.id}`">{{ props.status.account.display_name }}</RouterLink></b>
-        <div class="text-sm text-gray-500 dark:text-gray-400">@{{ props.status.account.acct }}</div>
-      </div>
+  <article class="p-5 bg-slate-700 space-y-2" v-if="baseStatus">
+    <div v-if="props.status.reblog !== null" class="flex flex-row space-x-1">
+      <FwbAvatar size="sm" rounded :img="props.status.account.avatar" />
+      <div class="text-sm">{{ props.status.account.display_name }} boosted</div>
     </div>
     
-    <Post v-if="props.status.reblog !== null" class="reblog" :status="props.status.reblog" />
-    <div v-else class="space-y-2">
-      <div v-html="props.status.content"></div>
+    <PostContent :status="baseStatus" />
+
+    <div class="space-y-2">
+      <div v-html="baseStatus.content"></div>
       <div class="flex flex-row justify-around">
-        <div>{{ props.status.replies_count }} comments</div>
-        <div>{{ props.status.favourites_count  }} likes</div>
-        <div>{{ props.status.reblogs_count }} boosts</div>
+        <div>{{ baseStatus.replies_count }} comments</div>
+        <div>{{ baseStatus.favourites_count  }} likes</div>
+        <div>{{ baseStatus.reblogs_count }} boosts</div>
       </div>
     </div>
   </article>
---
+++ src/components/PostContent.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+import { Status } from "@/api/entities/status";
+import Post from "@/components/Post.vue";
+import { FwbAvatar } from "flowbite-vue";
+
+const props = defineProps<{
+  status: Status
+}>();
+
+</script>
+
+<template>
+  <div class="flex items-center space-x-2 rtl:space-x-reverse">
+    <FwbAvatar size="md" :img="props.status.account.avatar" />
+    <div class="space-y-1 font-medium dark:text-white">
+      <b><RouterLink :to="`/user/${props.status.account.id}`">{{ props.status.account.display_name }}</RouterLink></b>
+      <div class="text-sm text-gray-500 dark:text-gray-400">@{{ props.status.account.acct }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
