2e586d48591bfae0e6d5ed8d8c6249313ef1bb1f
purifetchi <0xlunaric@gmail.com>
2024-04-30 12:00:33 +0200

    feat: implement infinite scroll.

---
+++ src/components/Intersector.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {onBeforeUnmount, onMounted, ref} from "vue";
+
+  const emit = defineEmits(["intersected"]);
+  const trigger = ref(null);
+
+  let handleIntersection = (entry) => {
+    if (entry.isIntersecting) {
+      emit('intersected');
+    }
+  };
+  
+  let observer;
+  onMounted(() => {
+    observer = new IntersectionObserver(entries => {
+      handleIntersection(entries[0]);      
+    }, {
+      type: Object,
+      default: {
+        root: null,
+        threshold: "0"
+      }
+    });
+    
+    observer.observe(trigger.value);
+  });
+  
+  onBeforeUnmount(() => {
+    observer.disconnect();
+  })
+</script>
+
+<template>
+  <span ref="trigger"></span>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -5,6 +5,7 @@ import { Status } from "@/api/entities/status";
 import Post from "@/components/Post.vue";
 import {FwbSpinner} from "flowbite-vue";
 import { useRouter } from "vue-router";
+import Intersector from "@/components/Intersector.vue";
 
 const router = useRouter();
 
@@ -21,14 +22,23 @@ onMounted(async () => {
 watch(() => props.endpoint, async () => {
   statuses.value = await call<Status[]>(props.endpoint);
 });
+
+let loadMore = async () => {
+  if (statuses.value === null) {
+    return;
+  }
+  
+  const more = await call<Status[]>(`${props.endpoint}?max_id=${statuses.value[statuses.value.length - 1].id}`);
+  statuses.value = statuses.value.concat(more);
+};
 </script>
 
 <template>
-  <div class="space-y-2.5">
-    <Post v-if="statuses !== null" v-for="status in statuses" :status="status" class="hover:cursor-pointer" @click="router.push(`/posts/${status.id}`)" />
-    <fwb-spinner v-else size="12" />
+  <div v-if="statuses !== null" class="space-y-2.5">
+    <Post v-for="status in statuses" :status="status" class="hover:cursor-pointer" @click="router.push(`/posts/${status.id}`)" />
+    <Intersector @intersected="loadMore()" />
   </div>
-  
+  <fwb-spinner v-else size="12" />
 </template>
 
 <style scoped>
