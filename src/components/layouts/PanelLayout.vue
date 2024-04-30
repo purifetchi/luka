883d425ddfe5657704b0c38ed4b28bcc43fdb407
purifetchi <0xlunaric@gmail.com>
2024-04-30 11:04:23 +0200

    feat: add a logout button.

--- src/components/layouts/PanelLayout.vue
+++ src/components/layouts/PanelLayout.vue
@@ -1,11 +1,19 @@
-<script setup>
+<script lang="ts" setup>
 
 import SidePanelUserCard from "@/components/SidePanelUserCard.vue";
+import {FwbButton} from "flowbite-vue";
+import {logout} from "@/api/mastodon";
+import { router } from "@/router/router.js";
+
+let doLogout = async () => {
+  await logout();
+  router.replace("/");
+};
 </script>
 
 <template>
   <div class="flex flex-row justify-around">
-    <aside>
+    <aside class="hidden md:block">
       <div class="sticky top-0">
         <h1>Luka</h1>
         <br>
@@ -14,12 +22,14 @@ import SidePanelUserCard from "@/components/SidePanelUserCard.vue";
         <RouterLink to="/timelines/home">Home</RouterLink>
         <br>
         <RouterLink to="/timelines/public">Public</RouterLink>
+        <br>
+        <fwb-button @click="doLogout()">Logout</fwb-button>
       </div>
     </aside>
-    <main class="max-w-2xl">
+    <main class="max-w-2xl w-full">
       <slot></slot>
     </main>
-    <aside>
+    <aside class="hidden md:block">
       <div class="sticky top-0">
         wah
       </div>
