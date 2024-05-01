8d801c5d5c6ae5868676dc27aedde9fb93651038
naomiEve <0xlunaric@gmail.com>
2024-05-01 12:24:13 +0200

    refactor: move the reply box to a separate component.

--- src/components/ReplyBox.vue
+++ src/components/ReplyBox.vue
@@ -1,9 +1,48 @@
-<script setup>
+<script lang="ts" setup>
 
+import {FwbButton, FwbCheckbox, FwbInput, FwbSelect, FwbTextarea} from "flowbite-vue";
+import { ref } from "vue";
+import { Status } from "@/api/entities/status";
+import { call } from "@/api/mastodon";
+
+const message = ref<string>("");
+const sensitive = ref<boolean>(false);
+
+const visibilities = [
+  { value: "public", name: "Public" },
+  { value: "unlisted", name: "Unlisted" },
+  { value: "private", name: "Followers Only" } 
+];
+
+const visibility = ref<string>("public");
+
+const props = defineProps<{
+  inReplyTo?: string
+}>();
+
+let sendPost = async () => {
+  let params = {
+    status: message.value,
+    media_ids: [],
+    visibility: visibility.value,
+    in_reply_to_id: props.inReplyTo,
+    sensitive: sensitive.value
+  };
+  
+  const resp = await call<Status>("/api/v1/statuses", params);
+};
 </script>
 
 <template>
-  $END$
+  <form v-on:submit.prevent="sendPost()">
+    <fwb-input v-if="sensitive" placeholder="Content warning (optional)" />
+    <fwb-textarea v-model="message" label="" placeholder="Just arrived in Shinonome Laboratories"></fwb-textarea>
+    <div class="flex flex-row justify-between">
+      <fwb-select :options="visibilities" v-model="visibility" required />
+      <fwb-checkbox v-model="sensitive">Sensitive</fwb-checkbox>
+      <fwb-button>Post</fwb-button>
+    </div>
+  </form>
 </template>
 
 <style scoped>
--- src/components/SidePanelUserCard.vue
+++ src/components/SidePanelUserCard.vue
@@ -1,12 +1,10 @@
 <script lang="ts" setup>
 import { store } from "@/store/store.js";
 import { call } from "@/api/mastodon";
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { Account } from "@/api/entities/account";
-import { FwbAvatar, FwbButton, FwbCard, FwbTextarea } from "flowbite-vue";
-import {Status} from "@/api/entities/status";
-
-const message = ref<string>("");
+import { FwbAvatar, FwbCard } from "flowbite-vue";
+import ReplyBox from "@/components/ReplyBox.vue";
 
 onMounted(async () => {
   if (store.self_account !== null) {
@@ -19,14 +17,6 @@ onMounted(async () => {
   
   store.self_account = await call<Account>("/api/v1/accounts/verify_credentials");
 });
-
-let sendPost = async () => {
-  const resp = await call<Status>("/api/v1/statuses", {
-    status: message.value,
-    media_ids: [],
-    visibility: "public"
-  });
-};
 </script>
 
 <template>
@@ -34,17 +24,16 @@ let sendPost = async () => {
   class="space-y-2"
   img-alt="Banner"
   :img-src="store.self_account.header">
-   <div class="flex items-center space-x-2 rtl:space-x-reverse">
-     <FwbAvatar size="lg" :img="store.self_account.avatar" />
-     <div class="space-y-1 font-medium dark:text-white">
-       <b><RouterLink :to="`/user/${store.self_account.id}`">{{ store.self_account.display_name }}</RouterLink></b>
-       <div class="text-sm text-gray-500 dark:text-gray-400">@{{ store.self_account.acct }}</div>
+   <div class="p-3 space-y-3">
+     <div class="flex items-center space-x-2 rtl:space-x-reverse">
+       <FwbAvatar size="lg" :img="store.self_account.avatar" />
+       <div class="space-y-1 font-medium dark:text-white">
+         <b><RouterLink :to="`/user/${store.self_account.id}`">{{ store.self_account.display_name }}</RouterLink></b>
+         <div class="text-sm text-gray-500 dark:text-gray-400">@{{ store.self_account.acct }}</div>
+       </div>
      </div>
+     <ReplyBox />
    </div>
-   <form v-on:submit.prevent="sendPost()">
-     <fwb-textarea v-model="message" label="" placeholder="Just arrived in Shinonome Laboratories"></fwb-textarea>
-     <fwb-button>Post</fwb-button>
-   </form>
  </fwb-card>
 </template>
 
