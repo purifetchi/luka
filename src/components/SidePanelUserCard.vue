3d24e145c195bd2ce9ef7ca9c2d6702f4da5dc31
purifetchi <0xlunaric@gmail.com>
2024-05-03 17:34:57 +0200

    feat: begin styling the side panel user card more.

--- src/components/SidePanelUserCard.vue
+++ src/components/SidePanelUserCard.vue
@@ -20,21 +20,18 @@ onMounted(async () => {
 </script>
 
 <template>
- <fwb-card v-if="store.self_account !== null"
-  class="space-y-2"
-  img-alt="Banner"
-  :img-src="store.self_account.header">
-   <div class="p-3 space-y-3">
-     <div class="flex items-center space-x-2 rtl:space-x-reverse">
-       <FwbAvatar size="lg" :img="store.self_account.avatar" />
-       <div class="space-y-1 font-medium dark:text-white">
-         <b><RouterLink :to="`/user/${store.self_account.id}`">{{ store.self_account.display_name }}</RouterLink></b>
-         <div class="text-sm text-gray-500 dark:text-gray-400">@{{ store.self_account.acct }}</div>
-       </div>
-     </div>
-     <ReplyBox />
-   </div>
- </fwb-card>
+ <div v-if="store.self_account !== null" class="space-y-3 border-[1px] border-slate-700">
+  <div class="bg-cover bg-center" :style="`background-image: url(${store.self_account.header});`">
+    <div class="backdrop-blur backdrop-brightness-50 flex items-center space-x-2 p-3 bg-gradient-to-b from-transparent to-slate-800">
+      <FwbAvatar size="lg" :img="store.self_account.avatar" />
+      <div class="space-y-1 font-medium dark:text-white">
+        <b><RouterLink :to="`/user/${store.self_account.id}`">{{ store.self_account.display_name }}</RouterLink></b>
+        <div class="text-sm text-gray-500 dark:text-gray-400">@{{ store.self_account.acct }}</div>
+      </div>
+    </div>
+  </div>
+  <ReplyBox />
+ </div>
 </template>
 
 <style scoped>
