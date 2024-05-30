04ee2d4347e74f122178a16c4232e8b030394e3d
naomiEve <0xlunaric@gmail.com>
2024-05-29 22:12:54 -0400

    feat: add an icon button.

---
+++ src/components/IconButton.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+const props = defineProps<{
+  icon: string,
+  toggle?: boolean
+  selected?: boolean
+}>();
+
+</script>
+
+<template>
+ <v-icon 
+     :name="props.icon" 
+     scale="1.5" 
+     :color="!props.selected && props.toggle ? '#ffffff80' : '#ffffff'" 
+     class="hover:cursor-pointer" 
+ />
+</template>
+
+<style scoped>
+
+</style>
