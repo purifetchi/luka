3842720f78b1e715e866513b34ccbec8e0990125
purifetchi <0xlunaric@gmail.com>
2024-06-09 23:41:59 +0200

    chore: styling fixes.

--- src/components/Intersector.vue
+++ src/components/Intersector.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onBeforeUnmount, onMounted, ref} from "vue";
+  import {onBeforeUnmount, onMounted, ref} from "vue";
 
   const emit = defineEmits(["intersected"]);
   const trigger = ref(null);
